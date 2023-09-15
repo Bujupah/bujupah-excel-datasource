5c2148dd52c5f6d992d771970d67a1da52bfb7e0
Khalil Mejdi <khalil_mejdi_tp@bmc.com>
2023-09-15 16:55:05 +0100

    Add LLM

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -4,18 +4,22 @@ import { QueryEditorProps } from '@grafana/data';
 import { DataSource } from '../datasource';
 import { MyDataSourceOptions, MyQuery } from '../types';
 import { RawEditor } from './query-editor/RawEditor';
+import { LoadingState } from '@grafana/schema';
 
 type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
 
-export function QueryEditor({ query, onChange, onRunQuery }: Props) {
-  return <RawEditor
-    query={query}
-    onChange={(q, p) => {
-      onChange(q)
-      if (p) {
-        onRunQuery()
-      }
-    }}
-    onRunQuery={onRunQuery}
-  />
+export function QueryEditor({ query, onChange, onRunQuery, data }: Props) {
+  return (
+    <RawEditor
+      query={query}
+      isRunning={data?.state === LoadingState.Loading}
+      onChange={(q, p) => {
+        onChange(q);
+        if (p) {
+          onRunQuery();
+        }
+      }}
+      onRunQuery={onRunQuery}
+    />
+  );
 }
